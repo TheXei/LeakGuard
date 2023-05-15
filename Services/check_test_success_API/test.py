bb8c8e0ca5d88f9fbd83574eeee83c37a4bfd6d1
Xei <jessen.marcus@gmail.com>
2023-05-15 12:56:51 +0200

    Opdateret test.py

--- Services/check_test_success_API/test.py
+++ Services/check_test_success_API/test.py
@@ -5,9 +5,16 @@ BASE = "http://127.0.0.1:5000/"
 data = [{"test_id": "0"}, {"test_id": "1"}, {"test_id": "2"}, 
         {"test_id": "3"}, {"test_id": "4"}, {"test_id": "5"}, {"test_id": "6"}]
 
-for i in range(len(data)):
-    response = requests.get(BASE + "check_test_success/test" + str(i))
-    print(response.json())
-
-# response = requests.get(BASE + "check_test_success/test0")
-# print(response.json())
+while True:
+    usrInput = input("Which test would you like to run? Press enter to run all tests, or 0 through 6 to run a specific test, then press enter again. Or enter 'exit' to exit. ")
+    if usrInput == "":
+        for i in range(len(data)):
+            response = requests.get(BASE + "check_test_success/test" + str(i))
+            print(response.json())
+    elif usrInput in ["0", "1", "2", "3", "4", "5", "6"]:
+        response = requests.get(BASE + "check_test_success/test" + str(usrInput))
+        print(response.json())
+    elif usrInput == "exit":
+        exit()
+    else:
+        print("Invalid input. Please try again. \n")
